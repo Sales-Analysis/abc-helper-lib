0b13e18fc6af68eafc1966c0c1d97aec51abca98
pandamy619 <vladislavtagaev@gmail.com>
2025-08-05 21:48:09 +0300

    refactor: mv types to types

--- abc/analysis.go
+++ abc/analysis.go
@@ -2,32 +2,32 @@ package abc
 
 import "fmt"
 
-type ABC struct {
-	SKU              []int
-	Name             []string
-	Quantity         []int
-	PriceUnit        []float64
-	PriceAll         []float64
-	ShareTotal       []float64
-	ShareAccumulated []float64
-	Group            []string
-}
-
 func New() *ABC {
 	return &ABC{}
 }
 
 func (a *ABC) Calculate(products []Product) int {
-	total := a.CalculatePriceTotal(products)
-	fmt.Println(total)
+	a.PriceTotal = a.calculatePriceTotal(products)
+	a.rankProductsByValue(products)
 	return 0
 }
 
 // Determining each product's contribution to total revenue
-func (a *ABC) CalculatePriceTotal(products []Product) []float64 {
+func (a *ABC) calculatePriceTotal(products []Product) []float64 {
 	total := []float64{}
 	for _, value := range products {
 		total = append(total, float64(value.Quantity)*value.Price)
 	}
 	return total
+}
+
+func (a *ABC) rankProductsByValue(products []Product) int {
+	// Привести к типу ABC, не изменять 'products []Product'
+	fmt.Println(a.PriceTotal)
+	indexes := []int{}
+	for i := range a.PriceTotal {
+		indexes = append(indexes, i)
+	}
+	fmt.Println(indexes)
+	return 0
 }
