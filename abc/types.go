8f9085926f639b674c410e98ff41185bdeac00dc
pandamy619 <vladislavtagaev@gmail.com>
2025-08-05 21:48:09 +0300

    refactor: mv types to types

--- abc/types.go
+++ abc/types.go
@@ -1,9 +1,26 @@
 package abc
 
+type ABC struct {
+	SKU              []int
+	Name             []string
+	Quantity         []int
+	PriceUnit        []float64
+	PriceTotal       []float64
+	ShareTotal       []float64
+	ShareAccumulated []float64
+	Group            []string
+}
+
 // Product is the input struct for the analysis.
 type Product struct {
 	SKU      string
 	Name     string
 	Quantity int
 	Price    float64
+}
+
+// pair is an unexported struct used to link a value with its original index for sorting.
+type pair struct {
+	value int
+	index int
 }
