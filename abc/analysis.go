aced7b7a3e9069bcec2ee1e1b530f9d6f5f492e0
pandamy619 <vladislavtagaev@gmail.com>
2025-08-06 16:12:02 +0300

    feat: rankProductsByValue

--- abc/analysis.go
+++ abc/analysis.go
@@ -1,6 +1,9 @@
 package abc
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func New() *ABC {
 	return &ABC{}
@@ -21,13 +24,30 @@ func (a *ABC) calculatePriceTotal(products []Product) []float64 {
 	return total
 }
 
+func (a byValue) Len() int           { return len(a) }
+func (a byValue) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byValue) Less(i, j int) bool { return a[i].value > a[j].value }
+
 func (a *ABC) rankProductsByValue(products []Product) int {
-	// Привести к типу ABC, не изменять 'products []Product'
-	fmt.Println(a.PriceTotal)
 	indexes := []int{}
 	for i := range a.PriceTotal {
 		indexes = append(indexes, i)
 	}
-	fmt.Println(indexes)
+
+	pairs := make(byValue, len(a.PriceTotal))
+
+	for i := 0; i < len(a.PriceTotal); i++ {
+		pairs[i] = pair{a.PriceTotal[i], indexes[i]}
+	}
+
+	sort.Sort(pairs)
+	for i, value := range pairs {
+		a.PriceTotal[i] = value.value
+		a.SKU = append(a.SKU, products[value.index].SKU)
+		a.Name = append(a.Name, products[value.index].Name)
+		a.Quantity = append(a.Quantity, products[value.index].Quantity)
+		a.PriceUnit = append(a.PriceUnit, products[value.index].Price)
+	}
+	fmt.Println(a)
 	return 0
 }
